f9ad76dfe34a2040741d5adb7958d4081c01ee58
Eduardo <edu.povas@gmail.com>
2020-07-30 22:03:01 -0300

    Home pronta e cadastros no basico

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './components/pages/home';
+import {BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './components/pages/cadstro/video'
+import CadastreoCategoria from './components/pages/cadstro/categoria';
+
+
+function Pagina404(){
+  return(
+    <div>
+      Erro 404
+    </div>
+  )
+}
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component= {Home} exact/>
+      <Route path="/cadastro/video" component= {CadastroVideo}/>
+      <Route path="/cadastro/categoria" component= {CadastreoCategoria}/>
+      <Route component= {Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
